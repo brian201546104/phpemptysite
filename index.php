0b5a6e38be98ff75b3d3a35d5a9d486643f182cb
WilliamLopez5 <willyslider@gmail.com>
2018-09-06 14:04:41 -0600

    V0.0.1
    Version Mejorada

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
 
-?>
+//incluir el archivo principal
+include("Slim/Slim.php");
+
+//registran la instancia de slim
+\Slim\Slim::registerAutoloader();
+//aplicacion 
+$app = new \Slim\Slim();
+
+//routing 
+//accediendo VIA URL
+//http:///www.google.com/
+//localhost/apirest/index.php/ => "Hola mundo ...."
+
+$app->get(
+    '/intento/:dato/:contr',function($estado,$contra) use ($app){
+    	$id = $estado;
+    	
+    	//almaceno el ID
+    	//conexion con base de datos
+    	//el proceso
+    	// retorno un JSON
+    	//$mensaje="la contraseña".$contra."es".$estado;
+       // mail("culpatanlopezl@gamil.com","intento de entrada al garage",$mensaje);
+        echo "la contraseña".$contra." es" . $estado;
+    }
+);
+
+//inicializamos la aplicacion(API)
+$app->run();
+
